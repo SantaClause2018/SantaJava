409ae7646e88703056352a4e7e708b9aba0f0f8b
Flavio D'Intino <flavio.dintino@gmail.com>
2018-12-10 21:49:32 +0100

    improve distance approx. test with random location

--- src/main/java/santa/DistanceTest.java
+++ src/main/java/santa/DistanceTest.java
@@ -1,15 +1,22 @@
 package santa;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class DistanceTest {
 	
 	
 	public static void runAccuracyTest(List<Gift> gifts, int testCnt) {
-			
+		Random random = new Random();
+		int giftCnt = gifts.size();
+		
 		for (int i=0; i< testCnt-1; i++) {
-			double exactDist = GiftPosition.getHaversineDistance(gifts.get(i).getPosition(), gifts.get(i+1).getPosition());
-			double approxDist =  GiftPosition.approximateHaversineDistance(gifts.get(i).getPosition(), gifts.get(i+1).getPosition());
+			int giftIds1 = random.nextInt(giftCnt);
+			int giftIds2 = random.nextInt(giftCnt);
+			double exactDist = GiftPosition.getHaversineDistance(gifts.get(giftIds1).getPosition(), gifts.get(giftIds2).getPosition());
+			double approxDist =  GiftPosition.approximateHaversineDistance(gifts.get(giftIds1).getPosition(), gifts.get(giftIds2).getPosition());
 			
 			System.out.println(toResultString(exactDist, approxDist, "%6.1f"));
 		}
@@ -22,15 +29,25 @@ public class DistanceTest {
 		long approxTime = 0;
 		
 		long exactStartTime = System.currentTimeMillis();
-		for (int i=0; i< testCnt-1; i++) {	
-			GiftPosition.getHaversineDistance(gifts.get(i).getPosition(), gifts.get(i+1).getPosition());		
+		HashMap<GiftPosition, GiftPosition>testGifts = new HashMap<GiftPosition, GiftPosition>();
+		Random random = new Random();
+		int giftCnt = gifts.size();
+		for (int i = 0; i< testCnt-1; i++) {
+			int giftIds1 = random.nextInt(giftCnt);
+			int giftIds2 = random.nextInt(giftCnt);
+			testGifts.put(gifts.get(giftIds1).getPosition(), gifts.get(giftIds2).getPosition());
+		}
+		
+		
+		for (Map.Entry<GiftPosition, GiftPosition> entry: testGifts.entrySet()) {	
+			GiftPosition.getHaversineDistance(entry.getKey(), entry.getValue());		
 		}
 		long exactEndTime = System.currentTimeMillis();
 		exactTime = exactEndTime - exactStartTime;
 		
 		long approxStartTime = System.currentTimeMillis();
-		for (int i=0; i< testCnt-1; i++) {
-			GiftPosition.approximateHaversineDistance(gifts.get(i).getPosition(), gifts.get(i+1).getPosition());
+		for (Map.Entry<GiftPosition, GiftPosition> entry: testGifts.entrySet()) {
+			GiftPosition.approximateHaversineDistance(entry.getKey(), entry.getValue());
 		}
 		long approxEndTime = System.currentTimeMillis();
 		approxTime = approxEndTime - approxStartTime;
--- src/main/java/santa/GiftPosition.java
+++ src/main/java/santa/GiftPosition.java
@@ -1,6 +1,8 @@
 package santa;
 
 public class GiftPosition {
+	
+	public static double APPROX_ACCURACY = 0.01;
 
     private double latitude;
     private double longitude;
@@ -36,7 +38,7 @@ public class GiftPosition {
     }
     
     public static double getHaversineDistance(GiftPosition giftPos1, GiftPosition giftPos2) {
-    	double EARTH_RADIUS = 6371.137; 
+    	double earthDiameter = 12742.274; 
     	
     	// calculate lat and log in rad
     	double phi1 = Math.toRadians(giftPos1.getLatitude());
@@ -45,12 +47,14 @@ public class GiftPosition {
     	double lambda2 = Math.toRadians(giftPos2.getLongitude());
   
         // apply formula
-        double a = Math.pow(Math.sin((phi2-phi1) / 2), 2) + Math.cos(phi1) * Math.cos(phi2) * Math.pow(Math.sin((lambda2-lambda1) / 2), 2); 
-        return  2 * EARTH_RADIUS * Math.asin(Math.sqrt(a)); 
+    	double sin1 = Math.sin((phi2-phi1) / 2);
+    	double sin2 = Math.sin((lambda2-lambda1) / 2);
+        double a = sin1*sin1 + Math.cos(phi1) * Math.cos(phi2) * sin2*sin2; 
+        return earthDiameter * Math.asin(Math.sqrt(a)); 
     }
     
     public static double approximateHaversineDistance(GiftPosition giftPos1, GiftPosition giftPos2) {
-    	double EARTH_RADIUS = 6371.137; 
+    	double earthDiameter = 12742.274; 
     	
     	// calculate lat and log in rad
     	double phi1 = Math.toRadians(giftPos1.getLatitude());
@@ -59,19 +63,11 @@ public class GiftPosition {
     	double lambda2 = Math.toRadians(giftPos2.getLongitude());
   
         // apply formula
-    	double sin1 = sinApprox((phi2-phi1) / 2, 0.01);
-    	double sin2 = sinApprox((lambda2-lambda1) / 2,0.01);   	
-        double radicand =  sin1*sin1 + cosApprox(phi1,0.01)*cosApprox(phi2,0.01)*sin2*sin2; 
+    	double sin1 = sinApprox((phi2-phi1) / 2, APPROX_ACCURACY);
+    	double sin2 = sinApprox((lambda2-lambda1) / 2,APPROX_ACCURACY);   	
+        double radicand =  sin1*sin1 + cosApprox(phi1,APPROX_ACCURACY)*cosApprox(phi2,APPROX_ACCURACY)*sin2*sin2; 
 
-        return  2 * EARTH_RADIUS * Math.asin(Math.sqrt(radicand)); 
-    }
-    
-    
-    // https://martin.ankerl.com/2007/10/04/optimized-pow-approximation-for-java-and-c-c/
-    private static double powApprox(final double a, final double b) {
-        final long tmp = Double.doubleToLongBits(a);
-        final long tmp2 = (long)(b * (tmp - 4606921280493453312L)) + 4606921280493453312L;
-        return Double.longBitsToDouble(tmp2);
+        return  earthDiameter * Math.asin(Math.sqrt(radicand)); 
     }
     
     private static double cosApprox(double x, double eps){
@@ -100,6 +96,19 @@ public class GiftPosition {
             if (i % 4 == 3) sum -= term;
         }
     	return sum;
+    }
+    
+    private static double asinApprox(double x, double eps ) {
+   	 // compute the Taylor series approximation
+   	 x = x % (2 * Math.PI);
+       double term = 1.0;      // ith term = x^i / i!
+       double sum  = 0.0;      // sum of first i terms in taylor series
+
+       for (int i = 1; Math.abs(term) >= eps; i++) {
+           term *= (x / i);
+           if (i % 4 == 1 || i % 4 == 3) sum += term;
+       }
+   	return sum;
     }
     
     
